c4f4834c9f2dde4b2c904d0ac19f46be3457b20e
nite12sinha <nite1220@gmail.com>
2025-07-28 10:30:37 +0530

    Add photo-editor-app/.next/static/chunks/bde8a_vimeo-video-element_dist_react_10e18df7.js 1753678837146

---
+++ photo-editor-app/.next/static/chunks/bde8a_vimeo-video-element_dist_react_10e18df7.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/bde8a_7ff7d523._.js"
+],
+    source: "dynamic"
+});
